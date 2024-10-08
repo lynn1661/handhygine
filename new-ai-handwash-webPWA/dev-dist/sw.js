e26c6f4274f1f148580fbab5d8a4ffff3419e20d
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2024-10-08 11:31:29 +0800

    http

--- new-ai-handwash-webPWA/dev-dist/sw.js
+++ new-ai-handwash-webPWA/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.th2qjh26ke8"
+    "revision": "0.aqcv6jm7lao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
