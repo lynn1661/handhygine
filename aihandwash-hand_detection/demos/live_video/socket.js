2e3e60703c327e8f1b4ca5eb7940d30515d6a4c4
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2024-10-14 15:44:47 +0800

    https

--- aihandwash-hand_detection/demos/live_video/socket.js
+++ aihandwash-hand_detection/demos/live_video/socket.js
@@ -7,7 +7,7 @@ export function createConnect(message) {
     return;
   } else if (message != null) {
     if (!socket) {
-      socket = io("https://handhygine.handhyine.com", {
+      socket = io("https://hapi.dogoo.co", {
         reconnection: true, // 允许重新连接
         reconnectionAttempts: Infinity, // 尝试无限次重新连接
       });
