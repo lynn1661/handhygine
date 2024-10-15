9ebcd6a1e33580ff137f6daf741f7a6336bdfcd0
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2024-10-15 10:26:01 +0800

    https

--- ai-handwash-assist-server-main/server.js
+++ ai-handwash-assist-server-main/server.js
@@ -1,9 +1,3 @@
 const server=require('micro-server');
 server.start({projectDir:__dirname});
 
-const cors = require('cors');
-app.use(cors({
-    origin: 'https://localhost',
-    methods: 'GET,POST',
-    credentials: true
-}));
