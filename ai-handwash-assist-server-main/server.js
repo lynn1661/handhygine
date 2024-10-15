a59556b6e73e2880103f27d5fe817c1062ed69dd
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2024-10-15 16:43:59 +0800

    cors

--- ai-handwash-assist-server-main/server.js
+++ ai-handwash-assist-server-main/server.js
@@ -1,3 +1,10 @@
 const server=require('micro-server');
 server.start({projectDir:__dirname});
 
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// 启用 CORS，允许来自所有源的请求
+app.use(cors());
+
--- new-ai-handwash-webPWA/dev-dist/sw.js
+++ new-ai-handwash-webPWA/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1gnm3ba59n8"
+    "revision": "0.uqqbudr6puo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
