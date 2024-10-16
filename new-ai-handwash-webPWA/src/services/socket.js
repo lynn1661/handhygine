f3618e22777aeef498c7ed0bc27a91459e05f9c4
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2024-10-16 08:25:07 +0800

    socket

--- new-ai-handwash-webPWA/src/services/socket.js
+++ new-ai-handwash-webPWA/src/services/socket.js
@@ -3,7 +3,7 @@ let socket = null;
 let res = null;
 export function createConnect(message) {
   if (!socket) {
-    socket = io("https://handhygine.handhyine.com/socket");
+    socket = io("https://handdetect.handhyine.com");
     socket.on("connect", () => {
       console.log("Socket connected");
       socket.on("message", (data) => {
