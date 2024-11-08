c1c2d36dc4f65327656dc91d5b0a9d6f2b710795
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2024-11-08 11:55:25 +0800

    Update socket.js

--- aihandwash-hand_detection/demos/live_video/socket.js
+++ aihandwash-hand_detection/demos/live_video/socket.js
@@ -8,7 +8,7 @@ export function createConnect(message) {
     return;
   } else if (message != null) {
     if (!socket) {
-      socket = io("https://realtime.handhyine.com", {
+      socket = io("https://ai.handhyine.com", {
         reconnection: true, // 允许重新连接
         reconnectionAttempts: Infinity, // 尝试无限次重新连接
       });
