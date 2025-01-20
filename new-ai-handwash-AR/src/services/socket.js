4115677a82b728628b3f046a5bb9281d4a270da9
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2025-01-20 12:50:40 +0800

    AR

---
+++ new-ai-handwash-AR/src/services/socket.js
@@ -0,0 +1,37 @@
+import { io } from "socket.io-client";
+let socket = null;
+let res = null;
+export function createConnect(message, currentStep) {
+  if (!socket) { // https://ai.handhyine.com or https://realtime.handhyine.com
+    socket = io("https://ai.handhyine.com");
+    socket.on("connect", () => {
+      console.log("Socket connected");
+      socket.on("message", (data) => {
+        res = data;
+        console.log('Received data from server:', data);
+      });
+    });
+    // 监听错误事件
+    socket.on("connect_error", (error) => {
+      console.error("Connection error:", error);
+    });
+    socket.on("disconnect", (reason) => {
+      console.log("Socket disconnected:"+ reason);
+    });
+  }
+  const payload = { data: message, step: currentStep };
+  socket.emit('message', payload);
+
+  return res;
+}
+
+export function handleData(data) {
+  return data;
+}
+
+export function disconnect() {
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+}
