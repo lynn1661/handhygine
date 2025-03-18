b5666d79ccb3271993f2a1a5e89e31ba137345ba
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2025-03-18 18:13:49 +0800

    login page

--- new-ai-handwash-APK/src/views/HandHygiene.vue
+++ new-ai-handwash-APK/src/views/HandHygiene.vue
@@ -21,51 +21,18 @@
             :placeholder="$t('HandHygiene.studentID')"
           />
         </div>
-        <div class="home-input-select">
-          <el-select
-            v-model="department"
-            class="selectOption"
-            :placeholder="$t('HandHygiene.department')"
-          >
-            <el-option
-              v-for="item in DepartmentOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-              class="custom-option"
-            />
-          </el-select>
+        <div class="home-input-password">
+          <el-input
+            v-model="studentID"
+            :placeholder="$t('HandHygiene.password')"
+          />
         </div>
-        <div class="home-input-select">
-          <el-select
-            v-model="programme"
-            class="selectOption"
-            :placeholder="$t('HandHygiene.programme')"
-          >
-            <el-option
-              v-for="item in programmeOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-              class="custom-option"
-            />
-          </el-select>
-        </div>
-        <div class="home-input-select">
-          <el-select
-            v-model="subject"
-            class="selectOption"
-            :placeholder="$t('HandHygiene.subject')"
-          >
-            <el-option
-              v-for="item in subjectOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-              class="custom-option"
-            />
-          </el-select>
-        </div>
+      </div>  
+      <div class="category">
+        <el-button plain>Doctor</el-button>
+        <el-button plain>Nurse</el-button>
+        <el-button plain>Assistant</el-button>
+        <el-button plain>Clerk</el-button>
       </div>
       <div class="home-btn">
         <el-button @click="started">{{ $t("HandHygiene.btn") }}</el-button>
@@ -128,147 +95,18 @@ import SelectLocale from "@/components/SelectLocale.vue";
 import { ElNotification } from "element-plus";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
-import { validateStudentID } from "../utils/formatData";
+//import { validateStudentID } from "../utils/formatData";
+import { ElAvatar } from 'element-plus';
+import { UserFilled } from '@element-plus/icons-vue';
+import { ElButton } from 'element-plus'
 const store = useStore();
 const router = useRouter();
 const t = useI18n();
-const department = ref("");
-const programme = ref("");
 const shouldChangeStyle = ref(false); // 默认不添加
-const DepartmentOptions = [
-  {
-    value: "SN",
-    label: "SN",
-  },
-  {
-    value: "SO",
-    label: "SO",
-  },
-  {
-    value: "HTI",
-    label: "HTI",
-  },
-  {
-    value: "ABCT",
-    label: "ABCT",
-  },
-  {
-    value: "Others",
-    label: "Others",
-  },
-];
-const subject = ref("");
-const programmeOptions = [
-  {
-    value: "BA(Hons)AASSM",
-    label: "BA(Hons)AASSM",
-  },
-  {
-    value: "BA(Hons)SW",
-    label: "BA(Hons)SW",
-  },
-  {
-    value: "BA(Hons)ASS (SW/SPSE)",
-    label: "BA(Hons)ASS (SW/SPSE)",
-  },
-  {
-    value: "BA(Hons)SPSE",
-    label: "BA(Hons)SPSE",
-  },
-  {
-    value: "BSc(Hons) MLS",
-    label: "BSc(Hons) MLS",
-  },
-  {
-    value: "BSc(Hons) Radiography",
-    label: "BSc(Hons) Radiography",
-  },
-  {
-    value: "BSc(Hons)OT",
-    label: "BSc(Hons)OT",
-  },
-  {
-    value: "BSc(Hons)PT",
-    label: "BSc(Hons)PT",
-  },
-  {
-    value: "BSc(Hons)Nursing",
-    label: "BSc(Hons)Nursing",
-  },
-  {
-    value: "BSc(Hons)MHN",
-    label: "BSc(Hons)MHN",
-  },
-  {
-    value: "BSc(Hons) Optometry",
-    label: "BSc(Hons) Optometry",
-  },
-  {
-    value: "BSc (Hons) in Vision Science + AIDA",
-    label: "BSc (Hons) in Vision Science + AIDA",
-  },
-  {
-    value: "BSc (Hons) in Vision Science + IE",
-    label: "BSc (Hons) in Vision Science + IE",
-  },
-  {
-    value: "BSc(Hons)ABCT",
-    label: "BSc(Hons)ABCT",
-  },
-  {
-    value: "BSc(Hons)ASTC",
-    label: "BSc(Hons)ASTC",
-  },
-  {
-    value: "DOptom",
-    label: "DOptom",
-  },
-  {
-    value: "Others",
-    label: "Others",
-  },
-];
-const subjectOptions = [
-  {
-    value: "JS3648",
-    label: "JS3648",
-  },
-  {
-    value: "JS3337",
-    label: "JS3337",
-  },
-  {
-    value: "52355-SY",
-    label: "52355-SY",
-  },
-  {
-    value: "ABCT1103",
-    label: "ABCT1103",
-  },
-  {
-    value: "SO4013",
-    label: "SO4013",
-  },
-  {
-    value: "SO4014",
-    label: "SO4014",
-  },
-  {
-    value: "Others",
-    label: "Others",
-  },
-];
 const studentID = ref("");
 const HandHygiene = ref(true);
 async function started() {
   try {
-    if (!validateStudentID(studentID.value)) {
-      ElNotification({
-        title: "Format：8個數字＋1英文字母（d/g/r）",
-        type: "error",
-      });
-      return;
-    }
     const res = await store.dispatch("user/login", {
       ID: studentID.value,
       subject: subject.value,
@@ -409,6 +247,23 @@ const backHome = () => {
         height: 60px;
       }
     }
+    &-password {
+      :deep(.el-input) {
+        width: 626px;
+        height: 90px;
+        margin: 25px;
+      }
+      :deep(.el-input__wrapper) {
+        background: #f5f8fd;
+        border-radius: 26px 26px 26px 26px;
+      }
+      :deep(.el-input__inner) {
+        font-family: "SourceHanSansCN";
+        font-size: 26px;
+        color: #b4c1d5;
+        height: 60px;
+      }
+    }
     &-select {
       :deep(.el-input) {
         width: 626px;
@@ -529,11 +384,18 @@ const backHome = () => {
     }
   }
 }
-
 .custom-option {
   color: rgba(108, 108, 108, 1);
   font-family: "SourceHanSansCN";
   font-size: 26px;
   height: 56px;
 }
+.avatar {
+  display: flex;
+}
+.avatar > div {
+  flex: 1;
+  text-align: center;
+}
+
 </style>
