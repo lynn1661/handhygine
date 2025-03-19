30dd3ce19fecffc443578b1429d7a28a31964706
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2025-03-19 14:21:21 +0800

    bcrypt

--- ai-handwash-assist-server-main/services/user/info.js
+++ ai-handwash-assist-server-main/services/user/info.js
@@ -1,6 +1,11 @@
 const microServer = require("micro-server");
 const { datap,utils } = microServer.helper;
 const isLogEnabled=require('micro-server').config.log===true;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// 异步加密密码
+const hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
 const fill=async({data})=>{
     if(Object.keys(data).indexOf('ID')<0 || Object.keys(data).indexOf('password')<0){
@@ -15,7 +20,7 @@ const fill=async({data})=>{
     }
     const obj={
         studentID:data.ID,
-        password:data.password,
+        password:hashedPassword,
         start_time:Date.now(),
     }
     const res=await datap.mongo.create('student_info',obj);
