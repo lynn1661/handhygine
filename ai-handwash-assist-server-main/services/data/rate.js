8e4cdf6c202ee14de6b26142f19d8ca709302a21
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2025-03-24 17:17:34 +0800

    update rate.js

--- ai-handwash-assist-server-main/services/data/rate.js
+++ ai-handwash-assist-server-main/services/data/rate.js
@@ -35,7 +35,11 @@ const rating = async ({ data }) => {
   };
 
   // 更新数据库记录
-  const updateRes = await datap.mongo.update("user_info", { _id: data.id }, { $set: updateObj });
+  const updateRes = await datap.mongo.update(
+    "user_info",
+    { _id: ObjectId(data.id) },
+    { $set: updateObj }
+  );
   if (!updateRes.acknowledged) {
     const err = new Error("Failed to update rating");
     err.code = 500;
