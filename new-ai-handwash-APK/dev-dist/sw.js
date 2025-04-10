e49b4a720400dd3ae36eda93e352fe01868b8417
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2025-04-10 17:05:05 +0800

    update vue step_points

--- new-ai-handwash-APK/dev-dist/sw.js
+++ new-ai-handwash-APK/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bsj7s0ot0d"
+    "revision": "0.s9s3rrcbqio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
