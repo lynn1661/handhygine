5c182752cb17aedff41ed94b4df1cb56a34629c4
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2025-05-08 16:40:37 +0800

    url

--- new-ai-handwash-APK/src/utils/axiosInstance.js
+++ new-ai-handwash-APK/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import router from "./../router";
 const axiosInstance = axios.create({ 
   baseURL: process.env.NODE_ENV === 'production' 
-    ? "/api/backend"  // 使用相对路径，不硬编码域名
+    ? "https://backend.polyuhandhygiene.com"  // 使用指定的后端服务地址  //? "/api/backend"  // 使用相对路径，不硬编码域名
     : "http://localhost:3000", // 本地开发环境使用localhost
   timeout: 1000 * 20,
 });
