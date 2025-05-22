beef674f5b3616f08fbce423c00c83ce92825908
lynn1661 <147331516+lynn1661@users.noreply.github.com>
2025-05-22 17:33:21 +0800

    update completion performance

--- new-ai-handwash-APK/dev-dist/sw.js
+++ new-ai-handwash-APK/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vi9lrm7vr3o"
+    "revision": "0.v2q86pvhri8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
